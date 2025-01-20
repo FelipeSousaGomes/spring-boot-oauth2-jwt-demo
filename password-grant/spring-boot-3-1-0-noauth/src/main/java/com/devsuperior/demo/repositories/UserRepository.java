99d2760b102c1226d0a2567583b151d6d98c0146
Felipe Sousa Gomes <felipesouzagomes21@gmail.com>
2025-01-19 21:59:53 -0300

    Feat: OAuth autorization not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/repositories/UserRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     @Query(nativeQuery = true, value = """
 			SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
 			FROM tb_user
